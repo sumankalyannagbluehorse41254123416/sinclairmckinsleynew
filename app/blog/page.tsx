63d64c5eb227d8b60b07f2af2fe3d0701a0c9036
santana <santanarana3@gmail.com>
2026-03-24 11:59:14 +0530

    change

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,46 @@
 import BlogCards from "@/components/blog/BlogCards";
+import { fetchPageData } from "@/service/fetchdata.services";
+import { headers } from "next/headers";
 
-export default function Blog() {
-  return (
-   <BlogCards />
-  );
+// ✅ Remove HTML tags
+const stripHtml = (html: string) => (html ? html.replace(/<[^>]*>/g, "") : "");
+
+// ✅ Types
+interface Section {
+  title?: string;
+  shortDescription?: string;
+  description?: string;
+  image?: string;
+  bannerImage?: string;
+  subsections?: Section[];
+  [key: string]: unknown;
+}
+
+interface SiteData {
+  pageItemdataWithSubsection?: Section[];
+  data?: {
+    pageItemdataWithSubsection?: Section[];
+  };
+}
+export default async function Blog() {
+  const rqHeaders = await headers();
+  const host = rqHeaders.get("host") || "localhost:3000";
+  const headersObj = Object.fromEntries(rqHeaders.entries());
+
+  let siteData: SiteData = {};
+
+  try {
+    siteData = await fetchPageData(
+      { host, ...headersObj },
+      "23c2fd3c-c8eb-4f09-8d3a-fdc0b909af8e",
+    );
+  } catch (error) {
+    console.error("Fetch error:", error);
+  }
+
+  const sections =
+    siteData.pageItemdataWithSubsection ||
+    siteData.data?.pageItemdataWithSubsection ||
+    [];
+  return  <BlogCards sections={sections} />;
 }
